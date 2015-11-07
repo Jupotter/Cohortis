301e813fc0a0052b7f57a3131d3c2dc19bd55d88
Julien Garagnon <jupotter.g@gmail.com>
2015-11-07 16:33:40 +0100

    Remove old project

--- src/Cohortis/ViewModels/ServerInfoViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cohortis.ViewModels
-{
-    public class ServerInfoViewModel
-    {
-        public string Name { get; set; }
-        public string LocalAddress { get; set; }
-        public string Software { get; set; }
-    }
-}
